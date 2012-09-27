a1f2abf2b1a264c531dc3f912c71f611327f9e5b
Rémi (halfr) Audebert <mail@halfr.net>
2012-09-27 08:17:49 +0200

    Add notify examples

---
+++ example/listen_date.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import time
+
+import sys
+sys.path.insert(0, '..')
+import cellaserv
+
+class EpochDelta(cellaserv.AsynClientDebug):
+    def __init__(self, sock):
+        super().__init__(sock=sock)
+        self.listen_notification('epoch')
+        self.connect_notify('epoch', self.on_epoch)
+
+    def on_epoch(self, message):
+        epoch = float(message['notify-data'])
+        print('{:3.3} msec'.format((time.time() - epoch) * 1000))
+
+if __name__ == "__main__":
+    import asyncore
+    import socket
+
+    import local_settings
+    HOST, PORT = local_settings.HOST, local_settings.PORT
+
+    service = EpochDelta(socket.create_connection((HOST, PORT)))
+    asyncore.loop()
---
+++ example/notify_date_service.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Use the cellaserv module to send notify every second"""
+
+import sys
+import time
+
+sys.path.append("..")
+import cellaserv
+
+class DateNotifier(cellaserv.SynClientDebug):
+    def __init__(self, sock):
+        super().__init__(sock=sock)
+
+    def broadcast_time(self):
+        self.notify('epoch', str(time.time()))
+
+    def run(self):
+        while not time.sleep(1):
+            self.broadcast_time()
+
+if __name__ == '__main__':
+    import socket
+
+    import local_settings
+    HOST, PORT = local_settings.HOST, local_settings.PORT
+
+    service = DateNotifier(socket.create_connection((HOST, PORT)))
+    service.run()
