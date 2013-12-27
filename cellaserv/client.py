ba53592d412e9f0127e87cd47df4a31d75027514
Rémi (halfr) Audebert <mail@halfr.net>
2013-12-27 18:57:26 +0100

    wip: client.py is using cellaserv2

--- cellaserv/client.py
+++ cellaserv/client.py
@@ -1,19 +1,28 @@
 """
 Python base class for writing clients for cellaserv.
 
-These classes only manipulates *messages* that are json objects. If you are
-looking for a high level API you should use ``cellaserv.service.Service``.
+These classes only manipulates protobuf *messages*. If you are looking for a
+high level API you should use ``cellaserv.service.Service``.
 
 Sample usage is provided in the ``example/`` folder of the source distribution.
 """
 
-__version__ = "1"
-
 import asynchat
-import json
-import uuid
+import logging
+import struct
 
 from collections import defaultdict
+
+from protobuf.cellaserv_pb2 import (
+        Message,
+        Register,
+        Request,
+        Reply,
+        Publish,
+        Subscribe
+)
+
+from google.protobuf.text_format import MessageToString
 
 class MessageTimeout(Exception):
     pass
@@ -25,265 +34,218 @@ class BadMessage(Exception):
     pass
 
 class AbstractClient:
-    """Abstract client"""
+    """Abstract client. Send protobuf messages."""
 
-    def __init__(self, sock, *args, **kwargs):
-        pass
+    def __init__(self):
+        # Nonce used to identify requests
+        self._request_seq_id = 0
 
-    def send_message(self, message, *args, **kwargs):
-        self._message_sent(message)
-
-        return self._send_message(message, *args, **kwargs)
+    def send_message(self, msg, *args, **kwargs):
+        self._send_message(msg.SerializeToString(), *args, **kwargs)
 
     def _send_message(self, *args, **kwargs):
+        """Implementation specific method for sending messages."""
         raise NotImplementedError
 
-    def _message_sent(self, message):
-        """Method call when a message is sent from the client.
+    ### Actions
 
-        Clients may redefine this method, for example for debugging purposes"""
-        self.message_sent(message)
+    def register(self, name, identification=None, *args, **kwargs):
+        """Send a ``register`` message.
 
-    def message_sent(self, message):
-        pass
+        :param str name: Name of the new service
+        :param str identification: Optional identification for the service
+        :rtype: None"""
 
-    def _message_recieved(self, message):
-        self.message_recieved(message)
 
-    def message_recieved(self, message):
-        pass
-
-    ### Commands
-
-    def register_service(self, name, identification=None, *args, **kwargs):
-        """Send a ``register`` command."""
-        message = {}
-        message['command'] = 'register'
-        message['service'] = name
+        register = Register(name=name)
         if identification:
-            message['identification'] = identification
-        message['id'] = str(uuid.uuid4())
+            register.identification = identification
+
+        message = Message(type=Message.Register,
+                          content=register.SerializeToString())
 
         self.send_message(message, *args, **kwargs)
 
-        return message['id']
+    def request(self, method, service, identification=None, data=None,
+            *args, **kwargs):
+        """Send a ``request`` message.
 
-    def query(self, action, to_service=None, to_identification=None,
-            data=None, *args, **kwargs):
-        """Send a ``query`` command.
+        :return: The message id
+        :rtype: int"""
 
-        Action comes first because every other argument is optional, eg.
-        you can broadcast a ping with ``client.query('ping')``."""
-
-        message = {}
-        message['command'] = 'query'
-        if to_service:
-            message['service'] = to_service
-        if to_identification:
-            message['identification'] = to_identification
+        request = Request(service_name=service, method=method)
+        if identification:
+            request.service_identification = identification
         if data:
-            message['data'] = data
-        message['action'] = action
-        message['id'] = str(uuid.uuid4())
+            request.data = data
+        request.id = self._request_seq_id
+        self._request_seq_id += 1
+
+        message = Message(type=Message.Request,
+                          content=request.SerializeToString())
 
         self.send_message(message, *args, **kwargs)
 
-        return message['id']
+        return request.id
 
-    def notify(self, event, event_data=None, *args, **kwargs):
-        """Send a ``notify`` command"""
-        message = {}
-        message['command'] = 'notify'
-        message['event'] = event
-        if event_data:
-            message['data'] = event_data
+    def publish(self, event, data=None, *args, **kwargs):
+        """Send a ``publish`` message"""
 
-        self.send_message(message, *args, **kwargs)
+        publish = Publish(event=event)
+        if data:
+            publish.data = data
 
-    def subscribe_event(self, event, *args, **kwargs):
-        """Send a ``subscribe`` command to register to an event"""
-        message = {}
-        message['command'] = 'subscribe'
-        message['event'] = event
+        message = Message(type=Message.Publish,
+                          content=publish.SerializeToString())
 
         self.send_message(message, *args, **kwargs)
 
-    def server(self, action, *args, **kwargs):
-        message = {}
-        message['command'] = 'server'
-        message['action'] = action
-        message['id'] = str(uuid.uuid4())
+    def subscribe(self, event, *args, **kwargs):
+        """Send a ``subscribe`` message."""
+
+        subscribe = Subscribe(event=event)
+
+        message = Message(type=Message.Subscribe,
+                          content=subscribe.SerializeToString())
 
         self.send_message(message, *args, **kwargs)
-
-        return message['id']
 
 class SynClient(AbstractClient):
     """Synchronous cellaserv client.
 
-    Checks if server version matches with library version when calling
-    ``__init__()``."""
+    Wait for ``response`` after every ``request`` message."""
 
     def __init__(self, sock):
-        super().__init__(sock=sock)
+        super().__init__()
 
         self._socket = sock
-        self._buffer = sock.makefile()
-        self._messages_waiting = {}
 
-    def _send_message(self, message, *args, **kwargs):
-        data = json.dumps(message).encode('ascii')
-        self._socket.send(data + b'\n')
+    def _send_message(self, msg, *args, **kwargs):
+        self._socket.send(struct.pack("!I", len(msg)))
+        self._socket.send(msg)
 
-    def _read_single_message(self):
-        resp = ""
-        while not resp:
-            resp = self._buffer.readline()
+    ### Actions
 
-        message = json.loads(resp)
+    def request(self, *args, **kwargs):
+        """Blocking ``request``."""
+        req_id = super().request(*args, **kwargs)
 
-        self._message_recieved(message)
+        while True:
+            # Receive message header
+            hdr = bytearray()
+            hdr_remaining = 4 # == struct.calcsize("I") == sizeof (uint32)
+            while hdr_remaining != 0:
+                recv_len = self._socket.recv_into(hdr, hdr_remaining)
+                hdr_remaining -= recv_len
+            # Header is the size of the message as a uint32 in network byte order
+            msg_len = struct.unpack("!I", hdr)
 
-        return message
+            # Receive message
+            msg = bytearray()
+            msg_remaining = msg_len
+            while msg_remaining != 0:
+                recv_len = self._socket.recv_into(msg, msg_remaining)
+                msg_remaining -= recv_len
 
-    def read_message(self, message_id=None):
-        """Read one message or read messages until ``message_id`` is found"""
-        if message_id:
-            while message_id not in self._messages_waiting:
-                new_message = self._read_single_message()
+            # Parse message
+            message = Message()
+            message.ParseFromString(msg)
+            if message.type != Message.Reply:
+                logging.warning("Received a non Reply message, dropping:")
+                logging.warning(MessageToString(message))
+                continue
 
-                if 'error' in new_message:
-                    raise ErrorMessage(new_message)
+            reply = Reply
+            reply.ParseFromString(message.content)
+            if reply.id != req_id:
+                logging.warning(
+                    "Received a Reply for the wrong Request, dropping:")
+                logging.warning(MessageToString(reply))
+                continue
 
-                try:
-                    self._messages_waiting[new_message['id']] = new_message
-                except KeyError as exc:
-                    raise BadMessage(new_message) from exc
-
-            message = self._messages_waiting.pop(message_id)
-
-            if (message['command'] == 'error'
-                and message['error-id'] == 'timeout'):
-                raise MessageTimeout(message_id)
-
-            return message
-
-        elif self._messages_waiting:
-            return self._messages_waiting.popitem()[1]
-
-        else:
-            return self._read_single_message()
-
-    ### Commands
-
-    def register_service(self, *args, **kwargs):
-        """ Blocking service register """
-        message_id = super().register_service(*args, **kwargs)
-
-        return self.read_message(message_id)
-
-    def query(self, *args, **kwargs):
-        """ Blocking query """
-        message_id = super().query(*args, **kwargs)
-
-        return self.read_message(message_id)
-
-    def server(self, action, *args, **kwargs):
-        message_id = super().server(action, *args, **kwargs)
-
-        return self.read_message(message_id)
-
-
-class SynClientDebug(SynClient):
-    """Synchronous debug client.
-
-    Prints mesages sent/recieved"""
-
-    def _message_recieved(self, message):
-        print("<< " + json.dumps(message))
-
-        super()._message_recieved(message)
-
-    def _message_sent(self, message):
-        print(">> " + json.dumps(message))
-
-        super()._message_sent(message)
-
+            return reply.data
 
 class AsynClient(asynchat.async_chat, AbstractClient):
     """Asynchronous cellaserv client."""
 
     def __init__(self, sock):
-        super().__init__(sock=sock)
-        AbstractClient.__init__(self, sock=sock)
+        # Init base classes
+        async_chat.async_chat().__init__(sock=sock)
+        AbstractClient.__init__(self)
 
-        # asyncore specific
-        self._ibuffer = []
-        self.set_terminator(b'\n')
+        # setup asynchat
+        self.set_terminator(4) # first, we are looking for a message header
+        # hold incoming data
+        self._ibuffer = bytearray()
+        self._read_header = True
 
-        self._ack_cb = None
-        self._event_cb = defaultdict(list)
+        # map events to a list of callbacks
+        self._events_cb = defaultdict(list)
 
     def _send_message(self, message, *args, **kwargs):
-        """Serialize and send a message (python dict) to the server"""
-        json_message = json.dumps(message).encode('ascii')
-        self.push(json_message + b'\n')
+        # 'push' is asynchat version of socket.send
+        self.push(struct.pack("!I", len(msg)))
+        self.push(msg)
 
     # Asyncore methods
 
     def collect_incoming_data(self, data):
-        """Store incoming data in a buffer before the terminator
-        string is recieved."""
-        self._ibuffer.append(data)
+        """Store incoming data in the buffer."""
+        self._ibuffer.extend(data)
 
     def found_terminator(self):
-        """Process incoming message (used by ``asyncore`` to handle incomming
-        socket data)."""
-        byte_data = b''.join(self._ibuffer)
-        self._ibuffer = []
-        json_message = byte_data.decode()
-        message = json.loads(json_message)
+        """Process an incoming header or message."""
 
-        self._message_recieved(message)
+        if self._read_header:
+            self._read_header = False
+
+            msg_len = struct.unpack("!I", self._ibuffer)
+            self.set_terminator(msg_len)
+
+            self._ibuffer.clear()
+        else:
+            self._read_header = True
+            self.set_terminator(4)
+
+            msg = Message()
+            msg.ParseFromString(self._ibuffer)
+
+            self._ibuffer.clear()
+
+            self._message_recieved(msg)
 
     # Methods called by subclasses
 
-    def set_ack_cb(self, f):
-        """Set the callback method called when a ``ack`` message is recieved."""
-        self._ack_cb = f
-
-    def connect_event(self, event, event_cb):
+    def add_subscribe_cb(self, event, event_cb):
         """On event ``event`` recieved, call ``event_cb``"""
-        self._event_cb[event].append(event_cb)
+        self._events_cb[event].append(event_cb)
 
     # Callbacks
 
-    def message_recieved(self, message):
-        """Called on incoming message from cellaserv"""
-        if 'command' in message:
-            if message['command'] == 'query':
-                self.query_recieved(message)
-            elif message['command'] == 'notify':
-                for cb in self._event_cb[message['event']]:
-                    cb(message)
-            elif message['command'] == 'ack' and self._ack_cb:
-                self._ack_cb(message)
+    def on_message_recieved(self, msg):
+        """Called on incoming message from cellaserv."""
+        if msg.type == Message.Request:
+            req = Request()
+            req.ParseFromString(msg.content)
+            self.recieved_request(req)
+        elif msg.type == Message.Reply:
+            rep = Reply()
+            rep.ParseFromString(msg.content)
+            self.received_reply(rep)
+        elif msg.type == Message.Publish:
+            pub = Publish()
+            pub.ParseFromString(msg.content)
+            for cb in self._events_cb[pub.event]:
+                if pub.HasField('data'):
+                    cb(pub.data)
+                else:
+                    cb()
+        else:
+            logging.warning("Invalid message:")
+            logging.warning(MessageToString(msg))
 
-    def query_recieved(self, query):
+    def on_request(self, req):
         pass
 
-
-class AsynClientDebug(AsynClient):
-    """Asynchronous debug client.
-
-    Prints mesages sent/recieved."""
-
-    def _message_recieved(self, message):
-        print("<< " + json.dumps(message))
-
-        super()._message_recieved(message)
-
-    def _message_sent(self, message):
-        print(">> " + json.dumps(message))
-
-        super()._message_sent(message)
+    def on_reply(self, rep):
+        pass
