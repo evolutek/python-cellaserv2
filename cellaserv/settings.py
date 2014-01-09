cd1b9527cf988947f218214885ed15f4582d09a6
Rémi (halfr) Audebert <mail@halfr.net>
2014-01-09 02:27:08 +0100

    settings: local_settings is not used

--- cellaserv/settings.py
+++ cellaserv/settings.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
 
+import configparser
 import logging
-
-try:
-    import local_settings
-    HOST, PORT = local_settings.HOST, local_settings.PORT
-    DEBUG = "0"
-except:
-    import os
-    import configparser
-
-    config = configparser.ConfigParser()
-    config.read(['/etc/conf.d/cellaserv'])
-
-    HOST = os.environ.get("CS_HOST",
-            config.get("client", "host", fallback="evolutek.org"))
-    PORT = int(os.environ.get("CS_PORT",
-        config.get("client", "port", fallback="4200")))
-
-    DEBUG = int(os.environ.get("CS_DEBUG",
-        config.get("client", "debug", fallback="0")))
+import os
+import socket
 
 logging.basicConfig()
 
+config = configparser.ConfigParser()
+config.read(['/etc/conf.d/cellaserv'])
+
+HOST = os.environ.get("CS_HOST",
+        config.get("client", "host", fallback="evolutek.org"))
+PORT = int(os.environ.get("CS_PORT",
+    config.get("client", "port", fallback="4200")))
+
+DEBUG = int(os.environ.get("CS_DEBUG",
+    config.get("client", "debug", fallback="0")))
+
 def get_socket():
-    import socket
     return socket.create_connection((HOST, PORT))
